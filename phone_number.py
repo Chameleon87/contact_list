1136d0352482d908025ba80a4539ef1d0f548547
Chameleon87 <jessehodge1987@gmail.com>
2016-04-24 04:58:15 -0500

    Made it more modular

--- phone_number.py
+++ phone_number.py
@@ -4,7 +4,9 @@ Contact App
 """
 import os
 import pickle
+#local imports
 from models import Contact
+from new_contact import new_contact_first_name, new_contact_last_name, new_contact_phone_number
 
 c = Contact()
 
@@ -22,37 +24,4 @@ def start():
             new_contact_first_name()
         else:
             start()
-
-
-def new_contact_first_name():
-    print "Please enter the first name of the contact you would like to add."
-    c.first_name = raw_input().capitalize()
-    print "Is " + c.first_name + " the correct first name?"
-    first_name_answer = raw_input().lower()
-    if first_name_answer == "yes":
-        new_contact_last_name()
-    else:
-        new_contact_first_name()
-
-def new_contact_last_name():
-    print "What's the last name of the person you're attempting to add?"
-    c.last_name = raw_input().capitalize()
-    print "Is " + c.last_name + " the name you're attempting to add?"
-    last_name_answer = raw_input().lower()
-    if last_name_answer == "yes":
-        new_contact_phone_number()
-
-
-def new_contact_phone_number():
-    a = "contact_list.txt"
-    print "Please enter the phone number for the contact you are attempting to add."
-    c.phone_number = raw_input().lower()
-    if len(c.phone_number) < 10:
-        print "please enter a phone number that contains 10 digits"
-    else:
-        NewContact = {c.first_name + ' ' + c.last_name: c.phone_number}
-        print NewContact
-        pickle.dump(NewContact, open(a, 'w'))
-        print "Save Successful!"
-
 start()
