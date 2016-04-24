45f562cbe1f8b6555a280e2a962ac37ee519886d
Chameleon87 <jessehodge1987@gmail.com>
2016-04-24 15:08:55 -0500

    Making big changes

--- phone_number.py
+++ phone_number.py
@@ -2,22 +2,22 @@
 """
 Contact App
 """
-#local imports
 from models import Contact
 from new_contact import new_contact_first_name
 
-c = Contact()
+contact = Contact()
+
 
 def start():
     print "Welcome to Contacts Plus!\n Would you like to take a look at all your contacts?"
     contacts_answer = raw_input().lower()
     if contacts_answer == "yes":
-        c.reading()
+        print contact
     else:
         print "Would you like to add a contact?"
         b = raw_input().lower()
         if b == "yes":
-            new_contact_first_name()
+            new_contact_first_name(contact)
         else:
             start()
 start()
