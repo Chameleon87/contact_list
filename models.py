de024fc1d46f52d52e35ff449a7978714bc79e6a
Chameleon87 <jessehodge1987@gmail.com>
2016-04-24 15:07:46 -0500

    Making changes

--- models.py
+++ models.py
@@ -1,26 +1,43 @@
 import os
 import pickle
 
-a = "contact_list.txt"
+DEFAULT_FILENAME = "contact_list.txt"
+
 
 class Contact(object):
-    def __init__(self):
+    def __init__(self, filename=None):
         self.first_name = ""
         self.last_name = ""
         self.phone_number = ""
+        if filename is None:
+            filename = DEFAULT_FILENAME
+        self.filename = filename
+        self.load()
 
-    def reading(self):
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print "Here is a list of all of your contacts saved so far:"
-        x = pickle.load(open(a, 'r'))
-        print x
+    def load(self):
+        if os.path.exists(self.filename):
+            with open(self.filename, 'r') as f:
+                try:
+                    loaded_dict = pickle.load(f)
+                except pickle.UnpicklingError:
+                    print "Invalid pickle file, loading failed"
 
-    def writing(self):
-        self.reading()
-        new_contact = {self.first_name + ' ' + self.last_name: self.phone_number}
-        new_dic = new_contact + self.reading()
-        print new_dic
-        pickle.dump(new_dic, open(a, 'wb'))
-        print "Save Successful!"
+            self.first_name = loaded_dict.get('first_name', '')
+            self.last_name = loaded_dict.get('last_name', '')
+            self.phone_number = loaded_dict.get('phone_number', '')
 
+    def __str__(self):
+        return "Full Name: {} {}\nPhone Number: {}".format(self.first_name, self.last_name, self.phone_number)
 
+    def save(self):
+        contact_dict = {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'phone_number': self.phone_number,
+        }
+        with open(self.filename, 'w') as f:
+            try:
+                pickle.dump(contact_dict, f)
+                print "Save Successful!"
+            except pickle.PickleError:
+                print "Cannot write to file"
