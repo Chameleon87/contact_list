b10ead74296a89e2c7fa7c6b4cdf90f22cb8fd87
Jesse Hodge <jessehodge1987@gmail.com>
2016-05-01 15:53:50 -0500

    Update models.py

--- models.py
+++ models.py
@@ -8,19 +8,6 @@ class Contact(object):
     def __str__(self):
         return "Full Name: {} {}\nPhone Number: {}".format(self.first_name, self.last_name, self.phone_number)
 
-    def save(self):
-        contact_dict = {
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'phone_number': self.phone_number,
-        }
-        with open(self.filename, 'w') as f:
-            try:
-                pickle.dump(contact_dict, f)
-                print "Save Successful!"
-            except pickle.PickleError:
-                print "Cannot write to file"
-
 
 class ContactList(object):
     def __init__(self, filename=None):
@@ -36,3 +23,16 @@ class ContactList(object):
                     loaded_dict = pickle.load(f)
                 except pickle.UnpicklingError:
                     print "Invalid pickle file, loading failed."
+
+    def save(self):
+        contact_dict = {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'phone_number': self.phone_number,
+        }
+        with open(self.filename, 'w') as f:
+            try:
+                pickle.dump(contact_dict, f)
+                print "Save Successful!"
+            except pickle.PickleError:
+                print "Cannot write to file"
